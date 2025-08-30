87af26dd9c7338838416aa7aa96b803feffa5a65
Ebru Serce <ebruserce@gmail.com>
2025-08-30 15:38:23 -0700

    headerbar

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,7 @@
+export default function HeaderBar() {
+    return (
+        <div className="w-full p-4 bg-red-600 text-white text-center text-2xl font-bold">
+            Pomogrowo
+        </div>
+    )
+}
